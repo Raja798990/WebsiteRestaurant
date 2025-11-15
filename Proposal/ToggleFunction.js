eb356c13a4f1df453fd4499e5200b17ec66022c9
gurdeep098 <g.gursimransingh@myauctioneer.eu>
2025-11-15 01:42:06 +0100

    "deleted toggle and google button cs"

--- Proposal/ToggleFunction.js
+++
@@ -1,9 +0,0 @@
-// Simple Show/Hide toggle for each proposal
-function toggleDetails(id) {
-  const section = document.getElementById(id);
-  if (section.style.display === "block") {
-    section.style.display = "none";
-  } else {
-    section.style.display = "block";
-  }
-}
